28f16b9beca8557a5bb0860aa551dccb43ddb6cf
jzhupan <jzhupan@gmail.com>
2023-06-09 12:59:22 -0400

    Implemented Mui library navigation bar, managed to add the page routers back.

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import LayoutPage from "./pages-tabs/layouts/Layout";
+import PrimarySearchAppBar from "./pages-tabs/layouts/components/NaviBar2";
 
 function App() {
   return (
     <div className="App">
-      <LayoutPage />
+      {/* <LayoutPage /> */}
+      <PrimarySearchAppBar />
     </div>
   );
 }
