67f122b3b4995733c3349639efb28f65ee98f26e
jzhupan <jzhupan@gmail.com>
2023-06-10 13:02:36 -0400

    Reverted the nav bar to a working state.

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 import LayoutPage from "./pages-tabs/layouts/Layout";
-import PrimarySearchAppBar from "./pages-tabs/layouts/components/NaviBar2";
+import PrimarySearchAppBar from "./pages-tabs/layouts/components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      {/* <LayoutPage /> */}
-      <PrimarySearchAppBar />
+      <LayoutPage />
     </div>
   );
 }
